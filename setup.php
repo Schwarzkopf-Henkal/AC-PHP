4257e9563048701c34858875d776aea8ee7f49b9
Schwarzkopf-Henkal <1198168788@qq.com>
2020-10-02 19:17:03 +0800

    Adjusted a little bit

--- setup.php
+++ setup.php
@@ -10,5 +10,16 @@ if(!$ConSQL){
 if(mysqli_query("CREATE DATABASE ac_db",$ConSQL)){
     echo $linetot++."Database created.<br>";
 }else echo $linetot++."Error creating database: ".mysqli_error($ConSQL);
+mysqli_select_db("ac_db",$ConSQL);
+$oper="CREATE TABLE users
+(
+    IdentityToken varchar(40),
+    AccessToken varchar(40),
+    UserName varchar(40),
+    Email varchar(40),
+    PhoneNum int,
+    UniqueID int
+)";
+mysqli_query($oper,$ConSQL);
 
 ?>
